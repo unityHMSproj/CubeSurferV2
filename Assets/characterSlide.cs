c3e29f11414083126545f82d4f3b3fc4b7053beb
Sselimkoc <selokoc.1@gmail.com>
2023-05-08 18:37:00 +0300

    radom

--- Assets/characterSlide.cs
+++ Assets/characterSlide.cs
@@ -6,6 +6,10 @@ public class characterSlide : MonoBehaviour
 {
     public static characterSlide instance;
     public List<GameObject> charList;
+
+    public GameObject lockscreen;
+
+    public float x_coor;
     public int id;
     void Awake()
     {
@@ -18,13 +22,17 @@ public class characterSlide : MonoBehaviour
     void Start()
     {
         id = 0;
+        x_coor = lockscreen.transform.position.x;
+
     }
 
     public void slideRight()
     {
         if (id < 7)
         {
+
             id++;
+            locked(charList[id]);
             foreach (GameObject model in charList)
             {
                 model.transform.position = new Vector3(model.transform.position.x + 5f, model.transform.position.y, model.transform.position.z);
@@ -37,11 +45,26 @@ public class characterSlide : MonoBehaviour
         if (id > 0)
         {
             id--;
-
+            locked(charList[id]);
             foreach (GameObject model in charList)
             {
                 model.transform.position = new Vector3(model.transform.position.x - 5f, model.transform.position.y, model.transform.position.z);
             }
         }
     }
-}
+
+     public void locked(GameObject model){   //karakterler bende kayıyor onun harici sorun yok ama belki sendede siyah ekran kaymıs olabılır
+
+
+        if (model.CompareTag("locked"))
+            {
+            lockscreen.transform.position = new Vector3(x_coor - 7f, lockscreen.transform.position.y, lockscreen.transform.position.z);
+        }
+        else
+            {
+            lockscreen.transform.position = new Vector3(x_coor , lockscreen.transform.position.y, lockscreen.transform.position.z);
+                }
+    
+
+}}
+
